b32ce25c43a38b8b3dcb1542c3b01b7c9d86d64e
Mike C. Fletcher <mcfletch@sturm>
2009-04-15 10:53:06 -0400

    Add some multi-element methods

--- vrml/olist.py
+++ vrml/olist.py
@@ -1,7 +1,15 @@
 """Observable list class"""
 from pydispatch.dispatcher import send
+import sets
+
 class OList( list ):
-	"""List sub-class which can generate pydispatch events on changes"""
+	"""List sub-class which can generate pydispatch events on changes
+	
+	Note that the OList semantics are a little loose currently, as 
+	it sometimes acts as though adding a new duplicate child is not 
+	an event and sometimes acts as though it is.  This doesn't cause 
+	problems for the OpenGLContext scenegraph.
+	"""
 	def append( self, value ):
 		"""Append a value and send a message"""
 		super( OList,self ).append( value )
@@ -24,20 +32,43 @@ class OList( list ):
 		super(OList,self).remove( item )
 		send( 'del', self, value = item )
 		return item
+	def __delitem__( self, index ):
+		"""Delete a single item"""
+		value = self[index]
+		send( 'del', self, value=value )
+		return value
+	def __delslice__( self, i,j ):
+		"""Delete a slice of sub-items"""
+		current = self[i:j]
+		for value in current:
+			send( 'del', self, value=value )
+		super( OList,self ).__delslice__( i,j )
+		return current 
 	def __setitem__( self, index, value ):
 		"""Set a value and send a message"""
-		send( 'del', self, value=self[index] )
+		current = self[index]
+		if current is not value:
+			send( 'del', self, value=current )
 		super( OList,self ).__setitem__( index, value )
-		send( 'new', self, value=value)
+		if current is not value:
+			send( 'new', self, value=value)
 		return value 
 	def __setslice__(self, i,j, iterable ):
 		"""Set values and send messages"""
-		for current in self.__getslice__( i,j ):
-			send( 'del', self, value=current )
 		values = list(iterable)
+		previous = self.__getslice__( i,j )
+		currents = sets.Set( previous )
 		super(OList,self).__setslice__( i,j, values )
 		for value in values:
-			send( 'new', self, value=value )
+			if value not in currents:
+				send( 'new', self, value=value )
+			else:
+				try:
+					previous.remove( value )
+				except ValueError, err:
+					pass
+		for current in previous:
+			send( 'del', self, value=current )
 		return values 
 	def __iadd__( self, iterable ):
 		"""Do an in-place add"""
@@ -57,7 +88,7 @@ if __name__ == "__main__":
 	del out[:]
 
 	o[0:2] = [ 'those','them' ]
-	assert out == [('del','this'),('new','those'),('new','them')], out
+	assert out == [('new','those'),('new','them'),('del','this'),], out
 	del out[:]
 	
 	o[1] = 'that'
@@ -76,3 +107,15 @@ if __name__ == "__main__":
 	o.remove( 'those' )
 	assert out == [('del','those')]
 	del out[:]
+	
+	
+	o[:] = [ 'those','them','their' ]
+	del out[:]
+	del o[1:2]
+	assert out == [('del','them')], out
+
+	o[:] = [ 'those','them','their','thou' ]
+	del out[:]
+	o[1:3] = ['them','those','that','them']
+	assert out == [('new','those'),('new','that'),('del','their')], out
+	
