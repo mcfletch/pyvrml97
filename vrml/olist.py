4dc5e8090d4032581024d7eadebda27a1e95956c
Mike C. Fletcher <mcfletch@sturm>
2009-04-15 11:20:57 -0400

    Make OList send both parent and child events, refactor the sending to be less messy

--- vrml/olist.py
+++ vrml/olist.py
@@ -3,55 +3,79 @@ from pydispatch.dispatcher import send
 import sets
 
 class OList( list ):
-	"""List sub-class which can generate pydispatch events on changes
+	"""List sub-class which generates pydispatch events on changes
 	
+	Generates 4 types of events:
+	
+		* NEW_CHILD_EVT, from self, with value=child, for each added child
+		* NEW_PARENT_EVT, from child, with parent=self, for each added child 
+		* DEL_CHILD_EVT, from self, with value=child, for each removed child 
+		* DEL_PARENT_EVT, from child, with parent=self, for each removed child
+		
 	Note that the OList semantics are a little loose currently, as 
 	it sometimes acts as though adding a new duplicate child is not 
 	an event and sometimes acts as though it is.  This doesn't cause 
 	problems for the OpenGLContext scenegraph.
+	
+	The OList is intended for situations where slow-write-fast-read 
+	is the primary requirement, it allows you to hook writing events 
+	in order to recalculate/cache values.
 	"""
+	NEW_CHILD_EVT = 'new'
+	NEW_PARENT_EVT = 'added'
+	DEL_CHILD_EVT = 'del'
+	DEL_PARENT_EVT = 'removed'
+	def _sendAdded( self, value ):
+		"""Send events for adding value to self"""
+		send( self.NEW_CHILD_EVT, self, value=value)
+		send( self.NEW_PARENT_EVT, value, parent=self)
+	def _sendRemoved( self, value ):
+		"""Send events for removing value from self"""
+		send( self.DEL_CHILD_EVT, self, value=value)
+		send( self.DEL_PARENT_EVT, value, parent=self)
+	
 	def append( self, value ):
 		"""Append a value and send a message"""
 		super( OList,self ).append( value )
-		send( 'new', self, value=value)
+		self._sendAdded( value )
 		return value 
 	def insert( self, index, value ):
 		"""Insert a new item at index"""
 		super( OList,self ).insert( index, value )
-		send( 'new', self, value=value)
+		self._sendAdded( value )
 		return value 
 	def pop( self, index=None ):
 		"""Pop a single item out of the list"""
 		if index is None:
 			index = len(self)-1
 		value = super( OList,self ).pop( index )
-		send( 'del', self, value = value )
+		self._sendRemoved( value )
 		return value
 	def remove( self, item ):
 		"""Remove this instance from the list"""
 		super(OList,self).remove( item )
-		send( 'del', self, value = item )
+		self._sendRemoved( item )
 		return item
 	def __delitem__( self, index ):
 		"""Delete a single item"""
 		value = self[index]
-		send( 'del', self, value=value )
+		self._sendRemoved( value )
 		return value
 	def __delslice__( self, i,j ):
 		"""Delete a slice of sub-items"""
 		current = self[i:j]
 		for value in current:
-			send( 'del', self, value=value )
+			self._sendRemoved( value )
 		super( OList,self ).__delslice__( i,j )
 		return current 
 	def __setitem__( self, index, value ):
 		"""Set a value and send a message"""
 		current = self[index]
 		if current is not value:
-			send( 'del', self, value=current )
+			self._sendRemoved( current )
 		super( OList,self ).__setitem__( index, value )
 		if current is not value:
-			send( 'new', self, value=value)
+			self._sendAdded( value )
 		return value 
 	def __setslice__(self, i,j, iterable ):
 		"""Set values and send messages"""
@@ -61,14 +85,14 @@ class OList( list ):
 		super(OList,self).__setslice__( i,j, values )
 		for value in values:
 			if value not in currents:
-				send( 'new', self, value=value )
+				self._sendAdded( value )
 			else:
 				try:
 					previous.remove( value )
 				except ValueError, err:
 					pass
 		for current in previous:
-			send( 'del', self, value=current )
+			self._sendRemoved( current )
 		return values 
 	def __iadd__( self, iterable ):
 		"""Do an in-place add"""
