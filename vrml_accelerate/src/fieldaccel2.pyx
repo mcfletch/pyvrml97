868ba083300f873b7f0dd37aa0470dacf8fa07cc
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-12-04 11:24:15 -0500

    Re-implement the field mechanism as raw descriptors with the old fset/fget/fdel as well

---
+++ vrml_accelerate/src/fieldaccel2.pyx
@@ -0,0 +1,90 @@
+from pydispatch.dispatcher import send
+_NULL = object()
+
+cdef class BaseField( object ):
+    cdef public str name
+    cdef public object defaultobj
+    cdef public int call_default
+    def __init__( self, str name, object default ):
+        self.name = name 
+        self.defaultobj = default
+        if hasattr(default, '__call__' ):
+            self.call_default = True
+        else:
+            self.call_default = False
+    def __get__( self, client, cls ):
+        """Retrieve value for given instance (or self for cls)"""
+        cdef dict idict
+        cdef object current 
+        if client is None:
+            return self 
+        idict = client.__dict__
+        current = idict.get( self.name, _NULL )
+        if current is _NULL:
+            return self.getDefault( client )
+        return current 
+    fget = __get__ 
+    def __set__( self, client, value ):
+        """Set value for given instance"""
+        value = self._set( client, value )
+        send( 
+            ('set',self), 
+            client, 
+            value=value,
+        )
+        #return value
+    def fset( self, client, value, int notify=True ):
+        value = self._set( client, value )
+        if notify:
+            send( 
+                ('set',self),
+                client, 
+                value=value,
+            )
+        return value
+    cdef _set(self, client, value ):
+        try:
+            value = self.coerce( value )
+        except ValueError, x:
+            raise ValueError( """Field %s could not accept value %s (%s)"""%( self, value, x))
+        except TypeError, x:
+            raise ValueError( """Field %s could not accept value %s of type %s (%s)"""%( self, value, type(value), x))
+        if isinstance( client, type ):
+            setattr( client, self.name, value )
+        else:
+            client.__dict__[self.name] = value
+        return value 
+    def __del__( self, client ):
+        """Delete our value from client's dictionary"""
+        try:
+            client.__dict__[ self.name ]
+        except KeyError, err:
+            raise AttributeError( self.name )
+    def fdel( self, client, notify=True ):
+        """Delete with notify"""
+        self.__del__( client )
+        send(
+            ('del',self), 
+            client, 
+        )
+        
+    def coerce( self, value ):
+        """Coerce the given value to our type"""
+        return value
+    def check( self, value ):
+        "Raise ValueError if isn't correct type"
+        return value
+    def getDefault( self, client = None ):
+        """Get the default value of this field
+
+        if client, set client's attribute to default
+        without sending a notification event.
+        """
+        cdef object defaultobj
+        if self.call_default:
+            defaultobj = self.defaultobj()
+        else:
+            defaultobj = self.defaultobj
+        if client is not None:
+            defaultobj = self._set( client, defaultobj )
+        return defaultobj
