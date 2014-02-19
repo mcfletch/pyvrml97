11c6bd8c18036e30ef3e6c3934f6f4c7b766330b
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-02-19 15:18:39 -0500

    Make very small something that is non-0 on 32-bit floats, more 0-div guards

--- vrml_accelerate/src/tmatrixaccel.pyx
+++ vrml_accelerate/src/tmatrixaccel.pyx
@@ -3,7 +3,7 @@ from __future__ import division
 import numpy as np
 cimport numpy as np
 
-cdef float VERY_SMALL = 1e-300
+cdef float VERY_SMALL = 1e-6
 cdef float TWOPI = np.pi * 2.0
 
 def transMatrix( float x=0.0, float y=0.0, float z=0.0 ):
@@ -84,8 +84,8 @@ def orthoMatrix( float left=-1.0, float right=1.0, float bottom=-1.0, float top=
     ty = - ( top + bottom ) / ( (top-bottom) or VERY_SMALL )
     tz = - ( zFar + zNear ) / ( (zFar-zNear) or VERY_SMALL )
     return np.array([
-        [2/(right-left),	0,	0,	 tx],
-        [0,	 2/(top-bottom),	0,	 ty],
-        [0,	0,	 -2/(zFar-zNear),	 tz],
+        [2/((right-left or VERY_SMALL)),	0,	0,	 tx],
+        [0,	 2/((top-bottom or VERY_SMALL)),	0,	 ty],
+        [0,	0,	 -2/((zFar-zNear or VERY_SMALL)),	 tz],
         [0,	0,	0,	1],
     ], dtype=np.float32)
