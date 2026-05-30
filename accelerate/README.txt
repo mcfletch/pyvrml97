af123acd2d98d967b44103f364ebcab423820f6a
Mike C. Fletcher <mcfletch@vrplumber.com>
2026-05-30 15:24:32 -0400

    PACKAGING Use pyproject.toml with a stub setup.py for the extension

--- accelerate/README.txt
+++ accelerate/README.txt
@@ -1,1 +1,16 @@
-Builds as a standard Python extension, is part of the PyVRML97 project under BSD license.
+PyVRML97-accelerate
+===================
+
+Scenegraph acceleration code for PyVRML97
+
+This set of C extensions provides acceleration of common operations
+within the PyVRML97 and OpenGLContext rendering engine.
+
+The extensions build as standard Python extensions. Cython is used to
+regenerate the C sources from the ``.pyx`` files when it is available;
+otherwise the prebuilt ``.c`` sources are compiled directly. The numpy
+headers are required to build the ``frustcullaccel`` and ``tmatrixaccel``
+modules.
+
+This package is part of the PyVRML97 project and is distributed under the
+BSD license (see license.txt for details).
--- accelerate/setup.py
+++ accelerate/setup.py
@@ -1,104 +1,55 @@
 #!/usr/bin/env python
-"""Builds accelleration functions for the VRML97 scenegraph
-"""
-try:
-    from setuptools import setup,Extension
-except ImportError as err:
-    from distutils.core import setup,Extension
-import sys, os
-SRC = 'src' 
-try:
-    from Cython.Distutils import build_ext
-except ImportError as err:
-    have_cython = False 
-else:
-    have_cython = True
+"""Builds acceleration functions for the VRML97 scenegraph
 
-def find_version( ):
-    for line in open( os.path.join('vrml_accelerate','__init__.py')):
-        if line.strip().startswith( '__version__' ):
-            return eval(line.strip().split('=')[1].strip())
-    raise RuntimeError( """No __version__ = 'string' in __init__.py""" )
-version = find_version()
+All project metadata lives in pyproject.toml; this file only declares the
+C/Cython extension modules, which cannot be expressed as static metadata.
+"""
+import os
+import sys
+from setuptools import setup, Extension
+
+SRC = "src"
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+def source(name):
+    """Return the .pyx source when Cython is available, else the prebuilt .c."""
+    suffix = ".pyx" if cythonize else ".c"
+    return os.path.join(SRC, name + suffix)
+
 
 extensions = [
-    Extension("vrml_accelerate.fieldaccel2", [
-            os.path.join( SRC, ["fieldaccel2.c","fieldaccel2.pyx"][have_cython])
-        ],
-    ),
+    Extension("vrml_accelerate.fieldaccel2", [source("fieldaccel2")]),
 ]
-    
 
 try:
     import numpy
-except ImportError as err:
+except ImportError:
     sys.stderr.write(
-        """Unable to import numpy, skipping numpy extension building\n"""
+        "Unable to import numpy, skipping numpy extension building\n"
     )
 else:
-    includeDirectories = [
-        numpy.get_include(),
-    ]
-    definitions = [
-        ('USE_NUMPY', True ),
-    ]
-    extensions.extend( [
-        Extension("vrml_accelerate.frustcullaccel", [
-                os.path.join( SRC, ["frustcullaccel.c","frustcullaccel.pyx"][have_cython])
-            ],
-            include_dirs = includeDirectories,
-            define_macros = definitions,
+    include_dirs = [numpy.get_include()]
+    extensions.extend([
+        Extension(
+            "vrml_accelerate.frustcullaccel",
+            [source("frustcullaccel")],
+            include_dirs=include_dirs,
+            define_macros=[("USE_NUMPY", "1")],
         ),
-        Extension("vrml_accelerate.tmatrixaccel", [
-                os.path.join( SRC, ["tmatrixaccel.c","tmatrixaccel.pyx"][have_cython])
-            ],
-            include_dirs = includeDirectories,
+        Extension(
+            "vrml_accelerate.tmatrixaccel",
+            [source("tmatrixaccel")],
+            include_dirs=include_dirs,
         ),
     ])
 
+if cythonize:
+    extensions = cythonize(extensions)
+
 if __name__ == "__main__":
-    extraArguments = {
-        'classifiers': [
-            """License :: OSI Approved :: BSD License""",
-            """Programming Language :: Python""",
-            """Programming Language :: C""",
-            """Topic :: Software Development :: Libraries :: Python Modules""",
-            """Topic :: Multimedia :: Graphics :: 3D Rendering""",
-            """Intended Audience :: Developers""",
-        ],
-        #'download_url': "https://sourceforge.net/projects/pyvrml97/files/PyVRML97/",
-        'keywords': 'VRML,VRML97,scenegraph,accelerate,fields',
-        'long_description' : """Scenegraph accelleration code for PyVRML97
-
-This set of C extensions provides accelleration of common operations
-within the PyVRML97 and OpenGLContext rendering engine.
-""",
-        'platforms': ['Win32','Linux','OS-X','Posix'],
-    }
-    ### Now the actual set up call
-    if have_cython:
-        extraArguments['cmdclass'] = {
-            'build_ext': build_ext,
-        }
-
-    setup (
-        name = "PyVRML97-accelerate",
-        version = version,
-        description = "Scenegraph acceleration code for PyVRML97",
-        author = "Mike C. Fletcher",
-        author_email = "mcfletch@vrplumber.com",
-        url = "http://pyopengl.sourceforge.net/context/",
-        license = "BSD-style, see license.txt for details",
-        packages = ['vrml_accelerate'],
-        # non python files of examples      
-        ext_modules=extensions,
-        options = {
-            'sdist': {
-                'formats': ['gztar','zip'],
-            },
-        },
-        package_dir = {
-            'vrml_accelerate':'vrml_accelerate',
-        },
-        **extraArguments
-    )
+    setup(ext_modules=extensions)
